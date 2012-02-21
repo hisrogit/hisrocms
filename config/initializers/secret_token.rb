2002ff950053f47f1146e948db1ca8d42adc1c0b
admin <admin@hisro.or.th>
2012-02-21 08:40:46 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hisrocms::Application.config.secret_token = '8390ca9b41a37d89172e0df98fa046eac1e639343934c0331cbe9cc3748d00181ed8998791cfa61ba2167016888cf85fca9b2477df4fa94d9a6d3f2d7bfbf27c'
